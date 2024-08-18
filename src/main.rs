c0cabf64560768c70d2d2528cd2894be9898cf58
项大维 <1744337932@qq.com>
2024-08-19 00:52:56 +0800

    delete unuseful code

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use clap::Parser;
 use rcli::{process_csv, Opts, SubCommand};
 
 fn main() -> anyhow::Result<()> {
-    print!("git pre commit test");
     let opts: Opts = Opts::parse();
     match opts.cmd {
         SubCommand::Csv(opts) => {

889198b23f2f90dbfd36c9f407a99c419f845d0e
项大维 <1744337932@qq.com>
2024-08-19 00:09:40 +0800

    Init project and implement the csv process

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// rcli csv -i input.csv -o output.json --header -d ','
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+
+
+fn main() -> anyhow::Result<()>{
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+        }
+    }
+
+    Ok(())
+}
+
---
+++ src/process.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use csv::Reader;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Player {
+    name: String,
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit: u8,
+
+}
+
+
+pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
+    let mut reader = Reader::from_path(input)?;
+    let mut ret = Vec::with_capacity(128);
+    for result in reader.deserialize() {
+        let record: Player = result?;
+        ret.push(record);
+    }
+
+    let json = serde_json::to_string_pretty(&ret)?;
+    fs::write(output, json)?;
+    Ok(())
+}
